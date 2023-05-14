8fa315a4040636a39eb09fec0d206f706f6a4cce
Pacey2706 <>
2023-05-14 20:47:28 +0000

    jdjdhjdhs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,24 @@ async function postForm(e) {
 
     const form = new FormData(document.getElementById("checksform"));
 
-    const response = await fetch(API_url, {
+    const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-            "Authorization": API_key,
+            "Authorization": API_KEY,
         },
         body: form,
     });
 
     const data = await response.json();
-    
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
 
 }
+
 
 async function getStatus(e) {
     const queryString = `${API_url}?api_key=${API_key}`;
@@ -35,6 +41,28 @@ async function getStatus(e) {
         throw new Error(data.error);
     }
 };
+
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
+
 function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = "API Key Status";
     document.getElementById("results-content").innerText = `Your key is valid until: ${data.expiry}`;
